d567181619ecd0a49609debf99f7fe8bfef956bc
= <christian.lazoespinoza@gmail.com>
2021-04-06 15:35:55 -0500

    init commit

---
+++ RandomSeisDigitos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace autoGenRandomCode
+{
+    class RandomSeisDigitos
+    {
+
+        private static RNGCryptoServiceProvider Rand =
+            new RNGCryptoServiceProvider();
+
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 3000; i++)
+            {
+                Console.WriteLine(GenerarSeisDigitosVerificacion(99999, 999999));
+            }
+
+            Console.Read();
+        }
+
+        private static int GenerarSeisDigitosVerificacion(int min, int max)
+        {
+            uint rango = uint.MaxValue;
+            while (rango == uint.MaxValue)
+            {
+                byte[] four_bytes = new byte[4];
+                Rand.GetBytes(four_bytes);
+
+                rango = BitConverter.ToUInt32(four_bytes, 0);
+            }
+
+            return (int)(min + (max - min) *
+                (rango / (double)uint.MaxValue));
+        }
+
+
+    }
+}
